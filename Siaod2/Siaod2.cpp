3b06740769ee77664c1d6a2241af4dee6ccc8fd2
Nord_Alx <poliektovma@gmail.com>
2024-10-02 01:08:50 +0300

    Pipu

--- Siaod2/Siaod2.cpp
+++ Siaod2/Siaod2.cpp
@@ -3,31 +3,39 @@
 #include <fstream>
 #include <random>
 #include <set>
-#include <algorithm>
 #include <vector>
 using namespace std;
 
-void make_file() {
-    ofstream file1("Text.txt");
+struct Table {
+    int key;
+    string name;
+};
+
+void dlt(vector <Table*> vec) {
+    while (vec.size() > 0) {
+        delete vec[0];
+        vec.erase(vec.begin());
+    }
+}
+
+void make_file(int n) {
+    ofstream file1("Text.bin");
     if (!file1.is_open())
         return;
 
-    int n, count = 0, number;
-    string names[5] = { "МИРЭА", "ВШЭ", "МГУ", "МИФИ", "МГТУ" };
+    int count = 0, number;
+    string names[9] = { "МИРЭА", "ВШЭ", "МГУ", "МИФИ", "МГТУ", "ПУП", "ПИП", "МИССИС", "ПИС" };
     set<int> st;
 
-    cout << "Input number of lines" << endl;
-    cin >> n;
-
     while (count < n) {
-        number = abs(rand()) % 10000;
+        number = abs(rand()) % 1000000;
         if (!st.count(number)) {
             st.insert(number);
             count++;
         }
     }
 
-    for (int x: st)
+    for (int x : st)
         file1 << x << " " << names[x % 5] << endl;
 
     file1.close();
@@ -36,11 +44,12 @@ void make_file() {
 int get_part(string line) {
     string num = "";
 
-    for (char x : line)
+    for (char x : line) {
         if (x == ' ')
             return stoi(num);
-        else
-            num += x;
+
+        num += x;
+    }
 }
 
 string get_another_part(string line) {
@@ -54,45 +63,104 @@ string get_another_part(string line) {
     return name;
 }
 
-void find_in_file() {
-    int key, mid, l = 0, r;
+string find_in_file(int key) {
+    int mid, l = 0, r;
     ifstream file("Text.bin");
     vector<string> vec;
     string line;
-    bool flag = false;
 
     if (!file.is_open())
-        return;
-
-    cout << "Input the key" << endl;
-    cin >> key;
+        return "NF";
     
     while (getline(file, line))
         vec.push_back(line);
+    file.close();
 
     r = vec.size() - 1;
 
-    while (l <= r && !flag) {
+    while (l <= r) {
         mid = (l + r) / 2;
 
-        if (get_part(vec[mid]) == key)
-            flag = true;
-        if (get_part(vec[mid]) > key)
+        if (get_part(vec[mid]) == key) 
+            return get_another_part(vec[mid]);
+        else if (get_part(vec[mid]) > key)
+            r = mid - 1;
+        else
+            l = mid + 1;
+    }
+    cout << "Not found" << endl;
+
+
+    return "NF";
+}
+
+Table* func1(int key, vector<Table*> unis, int n) {
+    int mid, l = 0, r = n - 1;
+
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (unis[mid] -> key == key) 
+            return unis[mid];
+        else if (unis[mid] -> key > key)
             r = mid - 1;
         else
             l = mid + 1;
     }
 
-    if (flag)
-        cout << get_another_part(vec[mid]);
-    else
-        cout << "Not found";
+    return NULL;
+}
 
+string func2(Table* unis) {
+    if (unis)
+        return unis -> name;
+    return "Not found";
+}
+
+void third_try(int n, int key) {
+    vector<Table*> unis;
+    string line;
+
+    ifstream file("Text.bin");
+    if (!file.is_open())
+        return;
+    
+    while (getline(file, line)) {
+        Table *table = new Table();
+        table -> key = get_part(line);
+        table -> name = get_another_part(line);
+        unis.push_back(table);
+    }
     file.close();
+
+    cout << func2(func1(key, unis, n)) << endl;
+
+    dlt(unis);
 }
 
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    find_in_file();
+
+    int n, key;
+    string name = "NF";
+
+    
+    cout << "Введите размер файла" << endl;
+    cin >> n;
+
+    cout << "Введите искомое значение" << endl;
+    cin >> key;
+
+    make_file(n);
+    
+    
+    double start_time = clock();
+    third_try(n, key);
+    double end_time = clock();
+    double search_time = end_time - start_time;
+    cout << "Программа работала " << search_time/1000 << " секунд" << endl;
+
+    
+    if (name != "NF")
+        cout << name;
 }
